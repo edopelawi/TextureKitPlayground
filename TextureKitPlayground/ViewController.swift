67fa7b2f2f5648a39c387b848ad20fba26768d2b
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-03 16:29:08 +0700

    Initial Commit

---
+++ TextureKitPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextureKitPlayground
+//
+//  Created by Ricardo Pramana Suranta on 03/07/18.
+//  Copyright © 2018 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
