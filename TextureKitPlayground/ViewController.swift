74f3a66fcc50816b727d67e3f88c522c80dfb555
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-06 13:26:47 +0700

    Initial attempt to make ViewController implements ASViewController

--- TextureKitPlayground/ViewController.swift
+++ TextureKitPlayground/ViewController.swift
@@ -7,19 +7,14 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
-class ViewController: UIViewController {
+final class ViewController: ASViewController <ASDisplayNode> {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
 
 }
 
