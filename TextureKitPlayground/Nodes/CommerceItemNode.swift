4e03f032b7e6d47906439f7b3477a5ceb6fa4d5e
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-07-30 14:00:49 +0700

    Initial commit fro CommerceItemNode

---
+++ TextureKitPlayground/Nodes/CommerceItemNode.swift
@@ -0,0 +1,26 @@
+//
+//  CommerceItemNode.swift
+//  TextureKitPlayground
+//
+//  Created by Ricardo Pramana Suranta on 26/07/18.
+//  Copyright © 2018 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+import AsyncDisplayKit
+
+final class CommerceItemNode: ASDisplayNode {
+
+	private let pictureNode = ASNetworkImageNode()
+	private let nameNode = ASTextNode()
+
+	override init() {
+		super.init()
+
+		automaticallyManagesSubnodes = true
+
+		// TODO: Implement this later
+	}
+
+
+}
